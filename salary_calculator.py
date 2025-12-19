71ebe729d4f66bf2ee9aa246c3b156cb540d041e
Felipe Dos Santos <99162735+Felipe-dSantos@users.noreply.github.com>
2025-12-18 20:13:25 -0500

    Create salary_calculator.py

---
+++ salary_calculator.py
@@ -0,0 +1,5 @@
+class SalaryCalculator:
+    def calculate_net_salary(self, gross_salary):
+        # Ainda não implementado (Fase RED)
+        # Retornamos None ou 0 para ver o teste falhar na asserção
+        return 0.0
